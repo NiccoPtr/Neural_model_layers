d1ff347791ffc82cc815b39e68923603e36cd30f
NiccPtr <petrucci.nikigmail.com>
2025-08-04 11:38:45 +0200

    Created test file for parameters

--- params.py
+++ params.py
@@ -3,8 +3,14 @@
 from parameter_manager import ParameterManager
 
 class Parameters(ParameterManager):
-    def __init__(self, bg_n=2):
-        self.bg_n = bg_n
+    def __init__(self, N, alpha, threshold, ö, alpha_uo, alpha_ui):
+        self.N = N
+        self.alpha = alpha
+        self.threshold = threshold
+        self.ö = ö
+        self.alpha_uo = alpha_uo
+        self.alpha_ui = alpha_ui
         super(Parameters, self).__init__()
+        
 
 
