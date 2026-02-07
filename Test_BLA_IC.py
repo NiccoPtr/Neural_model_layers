e63bac1840ac4173ef9a60210ca1a96285b77380
Nicc Petrucci <petrucci.niki@gmail.com>
2026-02-07 18:47:40 +0100

    Applied Francesco's editings to other simulations

--- Test_BLA_IC.py
+++ Test_BLA_IC.py
@@ -5,136 +5,123 @@ Created on Fri Feb  6 11:13:28 2026
 @author: Nicc
 """
 
-from params import Parameters
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 from Layer_types import BLA_IC_Layer
-import numpy as np, matplotlib.pyplot as plt
+from params import Parameters
 
-parameters = Parameters()
-parameters.load("prm_file.json" ,mode = "json")
-parameters.scheduling = {
-                        "trials": 10,
-                        "phases": [0.25, 0.50, 0.75, 1.0],
-                        "timesteps": 1000,
-                        "input": np.array([[0.2, 0.2, 0.0, 0.0, 0.0, 0.0],
-                                           [0.2, 0.2, 0.0, 0.0, 0.0, 0.0],
-                                           [0.2, 0.4, 0.0, 0.0, 0.0, 0.0],
-                                           [0.2, 0.4, 0.0, 0.0, 0.0, 0.0]]),
-                        'DA': np.array([0.0])
-                       }
-
-rng = np.random.RandomState(parameters.seed)
-
-BLA_IC = BLA_IC_Layer(parameters.N["BLA_IC"],
-                      parameters.tau["BLA_IC"][0],
-                      parameters.tau["BLA_IC"][1],
-                      parameters.baseline["BLA_IC"],
-                      rng,
-                      parameters.noise["BLA_IC"],
-                      parameters.BLA_Learn["eta_b"],
-                      parameters.BLA_Learn["tau_t"],
-                      parameters.BLA_Learn["alpha_t"],
-                      parameters.BLA_Learn["theta_DA"],
-                      parameters.BLA_Learn["max_W"])
-
-
-W = np.array([[5.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-              [0.0, 5.0, 0.0, 0.0, 0.0, 0.0],
-              [0.0, 0.0, 5.0, 0.0, -10.0, 0.0],
-              [0.0, 0.0, 0.0, 5.0, 0.0, -10.0]])
-
-results = []
-
-for trial in range(parameters.scheduling['trials']):
+def plotting(res):
     
+    fig, ax = plt.subplot(1, 1)
+    
+    BLA_IC = np.array(res['BLA_IC_output']) 
+    
+    ax.plot(BLA_IC[:, 0], label = 'Unit_1')
+    ax.plot(BLA_IC[:, 1], label = 'Unit_2')
+    ax.plot(BLA_IC[:, 2], label = 'Unit_3')
+    ax.plot(BLA_IC[:, 3], label = 'Unit_4')
+        
+    ax.set_title('BLA_IC simulation')
+    ax.legend()
+    ax.set_xlabel('Timestep')
+    ax.set_ylabel('Activity level')
+    ax.set_ylim(0, 1)
+
+    plt.show()    
+    
+def parse_args():
+    parser = argparse.ArgumentParser(description="BLA_IC simulation")
+    parser.add_argument(
+        "-p",
+        "--inp",
+        type=float,
+        nargs=6,
+        default=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        help="Input values (six floats)",
+    )
+    parser.add_argument(
+        "-t",
+        "--timesteps",
+        type=int,
+        default=1000,
+        help="Number of timesteps",
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        default="plot",
+        help="Output mode ('plot', 'save', 'stream')",
+    )
+    parser.add_argument(
+        "-n",
+        "--noise",
+        type=float,
+        default=0.0,
+        help="Insert BG_dl noise in simulation",
+    )
+    parser.add_argument(
+        '-da',
+        '--dopamine',
+        type=float,
+        default=0.0,
+        help='Insert Dopaminergic modulation for BLA_IC learning')
+    
+    return parser.parse_args()    
+
+if __name__ == "__main__":
+    args = parse_args()
+    inp = np.array(args.inp)
+    da = args.dopamine
+    timesteps = args.timesteps
+
+    parameters = Parameters()
+    parameters.load("prm_file.json" ,mode = "json")
+    parameters.noise['BLA_IC'] = args.noise
+
+    rng = np.random.RandomState(parameters.seed)
+    
+    BLA_IC = BLA_IC_Layer(parameters.N["BLA_IC"],
+                          parameters.tau["BLA_IC"][0],
+                          parameters.tau["BLA_IC"][1],
+                          parameters.baseline["BLA_IC"],
+                          rng,
+                          parameters.noise["BLA_IC"],
+                          parameters.BLA_Learn["eta_b"],
+                          parameters.BLA_Learn["tau_t"],
+                          parameters.BLA_Learn["alpha_t"],
+                          parameters.BLA_Learn["theta_DA"],
+                          parameters.BLA_Learn["max_W"])
+    
+    
+    W = np.array([[5.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, 5.0, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 5.0, 0.0, -10.0, 0.0],
+                  [0.0, 0.0, 0.0, 5.0, 0.0, -10.0]])
+        
     BLA_IC.reset_activity()
     BLA_IC_output = []
     _input_ = []
-    W_timeline = []
-    
-    if trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][0]:
-        phase = 1
-        
-    elif trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][1]:
-        phase = 2
-        
-    elif trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][2]:
-        phase = 3
-        
-    else: 
-        phase = 4
-        
-    inp = parameters.scheduling["input"][phase -1]
     
     for t in range(parameters.scheduling['timesteps']):
         
         BLA_IC.step(np.dot(W, inp))
-        BLA_IC.learn(parameters.scheduling['DA'])
+        BLA_IC.learn(da)
         
-        BLA_IC_output.append(np.round(BLA_IC.output.copy(), 4))
+        BLA_IC_output.append(BLA_IC.output.copy())
         _input_.append(inp.copy())
-        W_timeline.append(np.round(BLA_IC.W.copy(), 4))
 
-    result = {'Trial': trial + 1,
-              'Phase': phase,
+    result = {
               'BLA_IC_output': BLA_IC_output.copy(),
-              'Inputs_timeline': _input_.copy(),
-              'W': W_timeline.copy()
+              'Inputs_timeline': _input_.copy()
               }
     
-    results.append(result)
-
-def plotting(results):
-    
-    rows = 1 + len(results) // 2
-    cols = 2
-    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 4 * rows))
-    axs = axs.flatten()
-    
-    for i,res in enumerate(results):
-        
-        BLA_IC = np.array(res['BLA_IC_output']) 
-        
-        axs[i].plot(BLA_IC[:, 0], label = 'Unit_1')
-        axs[i].plot(BLA_IC[:, 1], label = 'Unit_2')
-        axs[i].plot(BLA_IC[:, 2], label = 'Unit_3')
-        axs[i].plot(BLA_IC[:, 3], label = 'Unit_4')
-        
-        axs[i].set_title(f'BLA_IC-Phase: {res["Phase"]}, Input: {res["Inputs_timeline"][0]}')
-        axs[i].legend()
-        axs[i].set_xlabel('Timestep')
-        axs[i].set_ylabel('Activity level')
-        axs[i].set_ylim(0, 1)
-
-    for j in range(len(results), len(axs)):
-        fig.delaxes(axs[j])
-
-    plt.tight_layout()
-    
-    plt.show()
-    
-    rows = 1 + len(results) // 2
-    cols = 2
-    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 4 * rows))
-    axs = axs.flatten()
-    
-    for i,res in enumerate(results):
-        
-        W = np.array(res['W']) 
-        n = W.shape[1]
-        for z in range(n):       
-            for j in range(n): 
-                axs[i].plot(W[:, z, j], label=f"{j+1}_{i+1}")
-                axs[i].set_title(f'Phase: {res["Phase"]}, Matrix learning')
-                axs[i].legend(loc = 'upper right')
-                axs[i].set_xlabel('Timestep')
-                axs[i].set_ylabel('Weight')
-                axs[i].set_ylim(0, parameters.BLA_Learn['max_W'])
-                
-    for j in range(len(results), len(axs)):
-        fig.delaxes(axs[j])
-
-    plt.tight_layout()
-    
-    plt.show()
-        
-        
+    if args.mode == "plot":
+        plotting(result)
+    elif args.mode == "stream":
+        mresults = np.hstack([result[key] for key in result.keys()])
+        for row in mresults:
+            print(("{:5.3f} " * len(row)).format(*row))
--- Testings_MGV_MC .py
+++ Testings_MGV_MC .py
@@ -5,88 +5,174 @@ Created on Mon Dec  8 10:59:11 2025
 @author: Nicc
 """
 
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Layer_types import BG_dl_Layer, Leaky_units_exc
 from params import Parameters
-from Layer_types import Leaky_units_exc, BG_dl_Layer
-import numpy as np, matplotlib.pyplot as plt
 
-parameters = Parameters()
-parameters.load("prm_file.json" ,mode = "json")
-parameters.scheduling = {
-                        "trials": 10,
-                        "phases": [0.25, 0.50, 0.75, 1.0],
-                        "timesteps": 1000,
-                        "input": np.array([[0.2, 0.2],
-                                           [0.2, 0.2],
-                                           [0.2, 0.4],
-                                           [0.2, 0.4]])
-                       }
-                                            
-rng = np.random.RandomState(parameters.seed)
+def plotting(res):
 
-BG_dl = BG_dl_Layer(parameters.N["BG_dl"], 
-                    parameters.tau["BG_dl"], 
-                    parameters.baseline["DLS"],
-                    parameters.baseline["STNdl"],
-                    parameters.baseline["GPi"],
-                    parameters.BG_dl_W["DLS_GPi_W"], 
-                    parameters.BG_dl_W["STNdl_GPi_W"],
-                    rng,
-                    parameters.noise["BG_dl"],
-                    parameters.threshold["BG_dl"])
+    fig, ax = plt.subplots(1, 1)
 
-MGV = Leaky_units_exc(parameters.N["MGV"], 
-                      parameters.tau["MGV"],
-                      parameters.baseline["MGV"],
-                      rng,
-                      parameters.noise["MGV"],
-                      parameters.threshold["MGV"])
+    BG_dl = np.array(res["BG_dl_output"]) * -1
 
-MC = Leaky_units_exc(parameters.N["MC"], 
-                     parameters.tau["MC"], 
-                     parameters.baseline["MC"],
-                     rng,
-                     parameters.noise["MC"],
-                     parameters.threshold["MC"])
+    ax.plot(BG_dl[:, 0], label="Unit_1")
+    ax.plot(BG_dl[:, 1], label="Unit_2")
 
-Ws = {"inp_DMS": np.eye(parameters.N["BG_dl"]),
-      "MC_MGV": np.eye(parameters.N["MGV"]) * parameters.Matrices_scalars["MC_MGV"],
-      "MGV_MC": np.eye(parameters.N["MC"]) * parameters.Matrices_scalars["MGV_MC"],
-      "GPi_MGV": np.eye(parameters.N["MGV"]) * parameters.Matrices_scalars["GPi_MGV"],
-      "MC_DLS": np.eye(parameters.N["BG_dl"]) * parameters.Matrices_scalars["MC_DLS"],
-      "MC_STNdl": np.eye(parameters.N["BG_dl"]) * parameters.Matrices_scalars["MC_STNdl"]
-      }
+    ax.set_title("BG simulation")
+    ax.legend()
+    ax.set_xlabel("Timestep")
+    ax.set_ylabel("Activity level")
+    ax.set_ylim(0, 1)
 
-BG_dl_output_pre = np.zeros(parameters.N["BG_dl"])
-MGV_output_pre = np.zeros(parameters.N["MGV"])
-MC_output_pre = np.zeros(parameters.N["MC"])
+    plt.tight_layout()
 
-results = []
-
-for trial in range(parameters.scheduling["trials"]):
+    plt.show()
     
-    BG_dl.reset_activity()
-    MGV.reset_activity()
-    MC.reset_activity()
+    fig, ax = plt.subplots(1, 1)
+
+    MGV = np.array(res["MGV_output"]) 
+
+    ax.plot(MGV[:, 0], label="Unit_1")
+    ax.plot(MGV[:, 1], label="Unit_2")
+
+    ax.set_title("MGV simulation")
+    ax.legend()
+    ax.set_xlabel("Timestep")
+    ax.set_ylabel("Activity level")
+    ax.set_ylim(0, 1)
+
+    plt.tight_layout()
+
+    plt.show()
+    
+    fig, ax = plt.subplots(1, 1)
+
+    MC = np.array(res["MC_output"]) * -1
+
+    ax.plot(MC[:, 0], label="Unit_1")
+    ax.plot(MC[:, 1], label="Unit_2")
+
+    ax.set_title("MC simulation")
+    ax.legend()
+    ax.set_xlabel("Timestep")
+    ax.set_ylabel("Activity level")
+    ax.set_ylim(0, 1)
+
+    plt.tight_layout()
+
+    plt.show()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="BG_dl-MGV-MC loop simulation")
+    parser.add_argument(
+        "-p",
+        "--inp",
+        type=float,
+        nargs=2,
+        default=[0.2, 0.2],
+        help="Input values (two floats)",
+    )
+    parser.add_argument(
+        "-t",
+        "--timesteps",
+        type=int,
+        default=1000,
+        help="Number of timesteps",
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        default="plot",
+        help="Output mode ('plot', 'save', 'stream')",
+    )
+    parser.add_argument(
+        "-nBG_DL",
+        "--noise_BG_dl",
+        type=float,
+        default=0.0,
+        help="Insert BG_dl noise in simulation",
+    )
+    parser.add_argument(
+        "-nMGV",
+        "--noise_MGV",
+        type=float,
+        default=0.0,
+        help="Insert MGV noise in simulation",
+    )
+    parser.add_argument(
+        "-nMC",
+        "--noise_MC",
+        type=float,
+        default=0.0,
+        help="Insert MC noise in simulation",
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    inp = np.array(args.inp)
+    timesteps = args.timesteps
+
+    parameters = Parameters()
+    parameters.load("prm_file.json" ,mode = "json")
+    parameters.noise['BG_dl'] = args.noise_BG_dl
+    parameters.noise['MGV'] = args.noise_MGV
+    parameters.noise['MC'] = args.noise_MC
+                                                
+    rng = np.random.RandomState(parameters.seed)
+    
+    BG_dl = BG_dl_Layer(parameters.N["BG_dl"], 
+                        parameters.tau["BG_dl"], 
+                        parameters.baseline["DLS"],
+                        parameters.baseline["STNdl"],
+                        parameters.baseline["GPi"],
+                        parameters.BG_dl_W["DLS_GPi_W"], 
+                        parameters.BG_dl_W["STNdl_GPi_W"],
+                        rng,
+                        parameters.noise["BG_dl"],
+                        parameters.threshold["BG_dl"])
+    
+    MGV = Leaky_units_exc(parameters.N["MGV"], 
+                          parameters.tau["MGV"],
+                          parameters.baseline["MGV"],
+                          rng,
+                          parameters.noise["MGV"],
+                          parameters.threshold["MGV"])
+    
+    MC = Leaky_units_exc(parameters.N["MC"], 
+                         parameters.tau["MC"], 
+                         parameters.baseline["MC"],
+                         rng,
+                         parameters.noise["MC"],
+                         parameters.threshold["MC"])
+    
+    Ws = {"inp_DMS": np.eye(parameters.N["BG_dl"]),
+          "MC_MGV": np.eye(parameters.N["MGV"]) * parameters.Matrices_scalars["MC_MGV"],
+          "MGV_MC": np.eye(parameters.N["MC"]) * parameters.Matrices_scalars["MGV_MC"],
+          "GPi_MGV": np.eye(parameters.N["MGV"]) * parameters.Matrices_scalars["GPi_MGV"],
+          "MC_DLS": np.eye(parameters.N["BG_dl"]) * parameters.Matrices_scalars["MC_DLS"],
+          "MC_STNdl": np.eye(parameters.N["BG_dl"]) * parameters.Matrices_scalars["MC_STNdl"]
+          }
+    
+    BG_dl_output_pre = np.zeros(parameters.N["BG_dl"])
+    MGV_output_pre = np.zeros(parameters.N["MGV"])
+    MC_output_pre = np.zeros(parameters.N["MC"])
+    
     BG_dl_output = []
     MGV_output = []
     MC_output = []
     _input_ = []
+        
+    BG_dl.reset_activity()
+    MGV.reset_activity()
+    MC.reset_activity()
     
-    if trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][0]:
-        phase = 1
-        
-    elif trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][1]:
-        phase = 2
-        
-    elif trial <= parameters.scheduling["trials"] * parameters.scheduling["phases"][2]:
-        phase = 3
-        
-    else: 
-        phase = 4
-        
-    inp = parameters.scheduling["input"][phase -1]
-    
-    for t in range(parameters.scheduling["timesteps"]):
+    for _ in range(timesteps):
     
         BG_dl.step(np.dot(Ws["inp_DMS"], inp),
                    np.dot(Ws["MC_DLS"], MC_output_pre),
@@ -103,97 +189,21 @@ for trial in range(parameters.scheduling["trials"]):
         
         MC_output_pre = MC.output.copy()
         
-        BG_dl_output.append(np.round(BG_dl.output_BG_dl.copy(), 4))
-        MGV_output.append(np.round(MGV.output.copy(), 4))
-        MC_output.append(np.round(MC.output.copy(), 4))
+        BG_dl_output.append(BG_dl.output_BG_dl.copy())
+        MGV_output.append(MGV.output.copy())
+        MC_output.append(MC.output.copy())
         _input_.append(inp.copy())
     
     result = {
-        "Trial": trial + 1,
-        "Phase": phase,
         "BG_dl_output": BG_dl_output.copy(),
         "MGV_output": MGV_output.copy(),
         "MC_output": MC_output.copy(),
         "Inputs_timeline": _input_.copy()
         }
     
-    results.append(result)
-    
-def plotting(results):
-    
-    rows = 1 + len(results) // 2
-    cols = 2
-    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 4 * rows))
-    axs = axs.flatten()
-    
-    for i,res in enumerate(results):
-        
-        BG_dl = np.array(res['BG_dl_output']) * -1
-        
-        axs[i].plot(BG_dl[:, 0], label = 'Unit_1')
-        axs[i].plot(BG_dl[:, 1], label = 'Unit_2')
-        
-        axs[i].set_title(f'BG_dl-Phase: {res["Phase"]}, Input: {res["Inputs_timeline"][0]}')
-        axs[i].legend()
-        axs[i].set_xlabel('Timestep')
-        axs[i].set_ylabel('Activity level')
-        axs[i].set_ylim(0, 1)
-
-    for j in range(len(results), len(axs)):
-        fig.delaxes(axs[j])
-
-    plt.tight_layout()
-    
-    plt.show()
-    
-    rows = 1 + len(results) // 2
-    cols = 2
-    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 4 * rows))
-    axs = axs.flatten()
-    
-    for i,res in enumerate(results):
-        
-        MGV = np.array(res['MGV_output'])
-        
-        axs[i].plot(MGV[:, 0], label = 'Unit_1')
-        axs[i].plot(MGV[:, 1], label = 'Unit_2')
-        
-        axs[i].set_title(f'MGV-Phase: {res["Phase"]}, Input: {res["Inputs_timeline"][0]}')
-        axs[i].legend()
-        axs[i].set_xlabel('Timestep')
-        axs[i].set_ylabel('Activity level')
-        axs[i].set_ylim(0, 1)
-
-    for j in range(len(results), len(axs)):
-        fig.delaxes(axs[j])
-
-    plt.tight_layout()
-    
-    plt.show()
-    
-    rows = 1 + len(results) // 2
-    cols = 2
-    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 4 * rows))
-    axs = axs.flatten()
-    
-    for i,res in enumerate(results):
-        
-        MC = np.array(res['MC_output'])
-        
-        axs[i].plot(MC[:, 0], label = 'Unit_1')
-        axs[i].plot(MC[:, 1], label = 'Unit_2')
-        
-        axs[i].set_title(f'MC-Phase: {res["Phase"]}, Input: {res["Inputs_timeline"][0]}')
-        axs[i].legend()
-        axs[i].set_xlabel('Timestep')
-        axs[i].set_ylabel('Activity level')
-        axs[i].set_ylim(0, 1)
-
-    for j in range(len(results), len(axs)):
-        fig.delaxes(axs[j])
-
-    plt.tight_layout()
-
-    plt.show()
-    
-
+    if args.mode == "plot":
+        plotting(result)
+    elif args.mode == "stream":
+        mresults = np.hstack([result[key] for key in result.keys()])
+        for row in mresults:
+            print(("{:5.3f} " * len(row)).format(*row))
