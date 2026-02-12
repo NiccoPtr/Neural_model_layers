e7860699ca3e1ec723c0e4284acb24c41c6dc10f
Francesco Mannella <francesco.mannella@gmail.com>
2026-02-12 15:13:11 +0100

    Mannella editing

--- Testings_MGV_MC .py
+++ Testings_MGV_MC .py
@@ -13,10 +13,13 @@ import numpy as np
 from CT_BG_simulation import CT_BG
 from params import Parameters
 
-#refactor: encapsulate an object of type CTBG which manages the used objects
+plt.ion()
+# refactor: encapsulate an object of type CTBG which manages the used objects
+
 
 def plotting(res):
 
+    plt.close("all")
     fig, ax = plt.subplots(1, 1)
 
     BG_dl = np.array(res["BG_dl_output"]) * -1
@@ -32,11 +35,11 @@ def plotting(res):
 
     plt.tight_layout()
 
-    plt.show()
-    
+    plt.pause(0.1)
+
     fig, ax = plt.subplots(1, 1)
 
-    MGV = np.array(res["MGV_output"]) 
+    MGV = np.array(res["MGV_output"])
 
     ax.plot(MGV[:, 0], label="Unit_1")
     ax.plot(MGV[:, 1], label="Unit_2")
@@ -50,7 +53,7 @@ def plotting(res):
     plt.tight_layout()
 
     plt.show()
-    
+
     fig, ax = plt.subplots(1, 1)
 
     MC = np.array(res["MC_output"])
@@ -67,21 +70,25 @@ def plotting(res):
     plt.tight_layout()
 
     plt.show()
-    
+
     fig, ax = plt.subplots(1, 1)
 
     W = np.array(res["Weight_timeline"])
-    
-    for i in range(2):
-        for j in range(2):
-            ax.plot(W[:, i, j], label=f'[{i}, {j}]')
-            
+
+    im = ax.imshow(
+        W.reshape(-1, 2 * 2).T, interpolation="none", aspect="auto", vmin=0, vmax=1
+    )
+    # for i in range(2):
+    #     for j in range(2):
+    #         ax.plot(W[:, i, j], label=f"[{i}, {j}]")
+
     ax.set_title("Weights learning")
     ax.legend()
     ax.set_xlabel("Timestep")
     ax.set_ylabel("Activity level")
-    ax.set_ylim(0, 1)
-    
+    ax.set_yticks(np.arange(4), [f"w{j}{i}" for j in range(2) for i in range(2)])
+    plt.colorbar(im, ax=ax)
+
     plt.tight_layout()
 
     plt.show()
@@ -89,6 +96,13 @@ def plotting(res):
 
 def parse_args():
     parser = argparse.ArgumentParser(description="BG_dl-MGV-MC loop simulation")
+    parser.add_argument(
+        "-s",
+        "--seed",
+        type=int,
+        default=0,
+        help="Seed for random number generation",
+    )
     parser.add_argument(
         "-p",
         "--inp",
@@ -115,8 +129,9 @@ def parse_args():
         "-da",
         "--dopamine",
         type=float,
+        nargs=2,
         default=[0.8, 0.8],
-        help="Insert dopamine for learnig: float type"
+        help="Insert dopamine for learnig: float type",
     )
     parser.add_argument(
         "-nBG_DL",
@@ -140,24 +155,16 @@ def parse_args():
         help="Insert MC noise in simulation",
     )
     parser.add_argument(
-        "--MC_MGV_W",
-        type=float,
-        default=3.0,
-        help="Insert MC_MGV matrix strenght"
+        "--MC_MGV_W", type=float, default=3.0, help="Insert MC_MGV matrix strenght"
     )
     parser.add_argument(
-        "--GPi_baseline",
-        type=float,
-        default=0.2,
-        help="Insert GPi baseline value"
+        "--GPi_baseline", type=float, default=0.2, help="Insert GPi baseline value"
     )
     parser.add_argument(
-        "--MGV_MC_W",
-        type=float,
-        default=1.0,
-        help="Insert MGV_MC matrix strenght"
+        "--MGV_MC_W", type=float, default=1.0, help="Insert MGV_MC matrix strenght"
     )
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -166,46 +173,49 @@ if __name__ == "__main__":
     da = np.array(args.dopamine)
 
     parameters = Parameters()
-    parameters.load("prm_file.json" ,mode = "json")
-    parameters.noise['BG_dl'] = args.noise_BG_dl
-    parameters.noise['MGV'] = args.noise_MGV
-    parameters.noise['MC'] = args.noise_MC
-    parameters.Matrices_scalars['MC_MGV'] = args.MC_MGV_W
-    parameters.Matrices_scalars['MGV_MC'] = args.MGV_MC_W
-    parameters.baseline['GPi'] = args.GPi_baseline
-                                                
+    parameters.load("prm_file.json", mode="json")
+    parameters.noise["BG_dl"] = args.noise_BG_dl
+    parameters.noise["MGV"] = args.noise_MGV
+    parameters.noise["MC"] = args.noise_MC
+    parameters.Matrices_scalars["MC_MGV"] = args.MC_MGV_W
+    parameters.Matrices_scalars["MGV_MC"] = args.MGV_MC_W
+    parameters.baseline["GPi"] = args.GPi_baseline
+    parameters.seed = args.seed
+    parameters.Str_Learn["eta_DLS"] = 0.2
+
     rng = np.random.RandomState(parameters.seed)
     CT_BG_model = CT_BG(parameters, rng)
-    
+
     BG_dl_output = []
     MGV_output = []
     MC_output = []
     W_timeline = []
     _input_ = []
-        
+
     CT_BG_model.reset_activity()
-    
+
     for _ in range(timesteps):
-    
+
         CT_BG_model.step(parameters, inp, da)
-        
+
         BG_dl_output.append(CT_BG_model.BG_dl.output_BG_dl.copy())
         MGV_output.append(CT_BG_model.MGV.output.copy())
         MC_output.append(CT_BG_model.MC.output.copy())
-        W_timeline.append(CT_BG_model.Ws['inp_DLS'].copy())
+        W_timeline.append(CT_BG_model.Ws["inp_DLS"].copy())
         _input_.append(inp.copy())
-    
+
     result = {
         "BG_dl_output": BG_dl_output.copy(),
         "MGV_output": MGV_output.copy(),
         "MC_output": MC_output.copy(),
         "Weight_timeline": W_timeline,
-        "Inputs_timeline": _input_.copy()
-        }
-    
+        "Inputs_timeline": _input_.copy(),
+    }
+
     if args.mode == "plot":
         plotting(result)
     elif args.mode == "stream":
         mresults = np.hstack([result[key] for key in result.keys()])
         for row in mresults:
             print(("{:5.3f} " * len(row)).format(*row))
+    input()
